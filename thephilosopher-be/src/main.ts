be083be273865cd104f626f390a04bda91427c3e
user <periklis@vitrinabox.com>
2023-07-23 23:23:32 +0300

    add port optinal

--- thephilosopher-be/src/main.ts
+++ thephilosopher-be/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api')
   app.enableCors()
-  await app.listen(3100);
+  await app.listen(process.env.PORT || 3100);
 }
 
 bootstrap();
