d6d6d541ae836e59e4297458879630327b38c10a
user <periklis@vitrinabox.com>
2023-07-24 00:23:36 +0300

    add base request

--- thephilosopher-be/src/main.ts
+++ thephilosopher-be/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api')
+  app.setGlobalPrefix('api', {
+    exclude: ['/']
+  })
   app.enableCors()
   await app.listen(process.env.PORT || 3100);
 }
