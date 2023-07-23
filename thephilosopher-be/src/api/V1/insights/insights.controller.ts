46eb56c26adfe4cca3982a88190dbfcc23919bad
user <periklis@vitrinabox.com>
2023-07-23 23:22:23 +0300

    thephilosopher-be: fix bug on search for match insights

--- thephilosopher-be/src/api/V1/insights/insights.controller.ts
+++ thephilosopher-be/src/api/V1/insights/insights.controller.ts
@@ -1,16 +1,21 @@
-import { Body, Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { InsightsService } from './insights.service';
 import { SupabaseGuard } from 'src/common/supabase';
 import { SearchEmbeddingsDto } from './insights.dto';
+import { AgentService } from 'src/modules/agent/agent.service';
 
 @UseGuards(SupabaseGuard)
 @Controller('v1/insights')
 export class InsightsController {
 
-    constructor(private readonly insightsService: InsightsService) { }
+    constructor(private readonly insightsService: InsightsService, private readonly agentService: AgentService) { }
 
     @Get('search')
     async searchInsights(@Query() reqBody: SearchEmbeddingsDto) {
         return this.insightsService.searchInsights(reqBody)
+    }
+    @Post('generate')
+    async generateInsight(@Query() reqBody: SearchEmbeddingsDto) {
+        await this.agentService.execute()
     }
 }
--- thephilosopher-be/src/api/V1/insights/insights.module.ts
+++ thephilosopher-be/src/api/V1/insights/insights.module.ts
@@ -4,9 +4,10 @@ import { InsightsController } from './insights.controller';
 import { SupabaseModule } from 'src/common/supabase';
 import { LangchainModule } from 'src/common/langchain/langchain.module';
 import { ConfigModule } from '@nestjs/config';
+import { AgentModule } from 'src/modules/agent/agent.module';
 
 @Module({
-  imports: [ConfigModule, SupabaseModule, LangchainModule],
+  imports: [ConfigModule, SupabaseModule, LangchainModule, AgentModule],
   providers: [InsightsService],
   controllers: [InsightsController]
 })
